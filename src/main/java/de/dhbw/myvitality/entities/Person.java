c669e264f04ebcfc8d49c182cab681ef320ac827
KanBen86 <benjamin.kanzler@gmail.com>
2018-10-08 12:37:14 +0200

    Id hinzugefügt und Annotationen ergänzt

--- src/main/java/de/dhbw/myvitality/entities/Person.java
+++ src/main/java/de/dhbw/myvitality/entities/Person.java
@@ -1,4 +1,13 @@
 package de.dhbw.myvitality.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Person {
+
+    @Id()
+    @GeneratedValue()
+    @Column()
+    private String personId;
 }
