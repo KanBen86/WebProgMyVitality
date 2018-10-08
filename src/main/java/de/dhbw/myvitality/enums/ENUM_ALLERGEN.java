69e509394c90d449cb9c18717e70cf9eb168b91e
KanBen86 <benjamin.kanzler@gmail.com>
2018-10-08 13:56:56 +0200

    neues ENUM für die Allergene eines Artikels

---
+++ src/main/java/de/dhbw/myvitality/enums/ENUM_ALLERGEN.java
@@ -0,0 +1,11 @@
+package de.dhbw.myvitality.enums;
+
+public enum ENUM_ALLERGEN {
+    LACTOSE("Lactose");
+
+    private String description;
+
+    ENUM_ALLERGEN(String description) {
+        this.description = description;
+    }
+}
