c89a76bc76ef0f0b697edb561f36637cda992d52
Sven <sven@hornung.email>
2018-10-09 14:53:45 +0200

    Für die Klasse TypeOfPayment wurden die Konsturktoren jeweils mit und ohne alle Variablen hinzugefügt und alle getter und setter Methoden wurden ergänzt

--- src/main/java/de/dhbw/myvitality/entities/TypeOfPayment.java
+++ src/main/java/de/dhbw/myvitality/entities/TypeOfPayment.java
@@ -1,6 +1,5 @@
 package de.dhbw.myvitality.entities;
 
-import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -15,4 +14,55 @@ public class TypeOfPayment {
     private String cardNo;
 
     private LocalDate expirationDate;
+
+    public TypeOfPayment(List<PaymentMethod> type, String paymentEmail, Bank bank, String cardNo, LocalDate expirationDate) {
+        this.type = type;
+        this.paymentEmail = paymentEmail;
+        this.bank = bank;
+        this.cardNo = cardNo;
+        this.expirationDate = expirationDate;
+    }
+
+    public TypeOfPayment() {
+    }
+
+    public List<PaymentMethod> getType() {
+        return type;
+    }
+
+    public void setType(List<PaymentMethod> type) {
+        this.type = type;
+    }
+
+    public String getPaymentEmail() {
+        return paymentEmail;
+    }
+
+    public void setPaymentEmail(String paymentEmail) {
+        this.paymentEmail = paymentEmail;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public String getCardNo() {
+        return cardNo;
+    }
+
+    public void setCardNo(String cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 }
