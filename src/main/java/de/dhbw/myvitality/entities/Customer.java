4dbae619659a455090bae3ee1def1a9ddc4ee909
Sven <sven@hornung.email>
2018-10-09 14:40:09 +0200

    Für die Klasse Customer nicht benötigte Imports gelöscht, ein Konstruktor mit allen und ein Konstruktor ohne alle Variablen hinzugefügt sowie alle getter und setter Methoden hinzugefügt

--- src/main/java/de/dhbw/myvitality/entities/Customer.java
+++ src/main/java/de/dhbw/myvitality/entities/Customer.java
@@ -1,6 +1,5 @@
 package de.dhbw.myvitality.entities;
 
-import javax.persistence.*;
 import java.util.List;
 
 public class Customer {
@@ -18,4 +17,73 @@ public class Customer {
     private FitnessLevel fitnessLevel;
 
     private String preferredTypeOfPayment;
+
+    public Customer(String customerId, Address delivieryAddress, Address billAddress, Goal goal, List<TrainingSchedule> trainingPlan, FitnessLevel fitnessLevel, String preferredTypeOfPayment) {
+        this.customerId = customerId;
+        this.delivieryAddress = delivieryAddress;
+        this.billAddress = billAddress;
+        this.goal = goal;
+        this.trainingPlan = trainingPlan;
+        this.fitnessLevel = fitnessLevel;
+        this.preferredTypeOfPayment = preferredTypeOfPayment;
+    }
+
+    public Customer() {
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public Address getDelivieryAddress() {
+        return delivieryAddress;
+    }
+
+    public void setDelivieryAddress(Address delivieryAddress) {
+        this.delivieryAddress = delivieryAddress;
+    }
+
+    public Address getBillAddress() {
+        return billAddress;
+    }
+
+    public void setBillAddress(Address billAddress) {
+        this.billAddress = billAddress;
+    }
+
+    public Goal getGoal() {
+        return goal;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
+
+    public List<TrainingSchedule> getTrainingPlan() {
+        return trainingPlan;
+    }
+
+    public void setTrainingPlan(List<TrainingSchedule> trainingPlan) {
+        this.trainingPlan = trainingPlan;
+    }
+
+    public FitnessLevel getFitnessLevel() {
+        return fitnessLevel;
+    }
+
+    public void setFitnessLevel(FitnessLevel fitnessLevel) {
+        this.fitnessLevel = fitnessLevel;
+    }
+
+    public String getPreferredTypeOfPayment() {
+        return preferredTypeOfPayment;
+    }
+
+    public void setPreferredTypeOfPayment(String preferredTypeOfPayment) {
+        this.preferredTypeOfPayment = preferredTypeOfPayment;
+    }
 }
