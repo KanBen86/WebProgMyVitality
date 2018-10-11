a42b0eb941a764217afe9ed5be07f79d1ab46f95
tamino.fischer <fischer.tamino@dh-karlsruhe.de>
2018-10-11 14:49:49 +0200

    Interface AdressRepository für Spring JPA erstellt.

---
+++ src/main/java/de/dhbw/myvitality/repositories/AdressRepository.java
@@ -0,0 +1,6 @@
+package de.dhbw.myvitality.repositories;
+
+import de.dhbw.myvitality.entities.Adress;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdressRepository extends CrudRepository<Adress, String> { }
