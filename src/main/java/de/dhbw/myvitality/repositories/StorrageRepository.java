68c86f104e4c0ffe4fdebbed702e8e82eadd4073
KanBen86 <benjamin.kanzler@gmail.com>
2018-10-11 10:59:53 +0200

    Repositories für die Artikel und das Lager

---
+++ src/main/java/de/dhbw/myvitality/repositories/StorrageRepository.java
@@ -0,0 +1,7 @@
+package de.dhbw.myvitality.repositories;
+
+import de.dhbw.myvitality.entities.Storrage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StorrageRepository extends CrudRepository<Storrage, String> {
+}
