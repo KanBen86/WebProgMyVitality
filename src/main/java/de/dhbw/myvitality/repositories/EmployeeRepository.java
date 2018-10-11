8c04a1dd9b62c5d05ff861623b0c0f18bf5c5181
KanBen86 <benjamin.kanzler@gmail.com>
2018-10-11 10:27:36 +0200

    Re

--- src/main/java/de/dhbw/myvitality/repositories/EmployeeRepository.java
+++ src/main/java/de/dhbw/myvitality/repositories/EmployeeRepository.java
@@ -3,8 +3,5 @@ package de.dhbw.myvitality.repositories;
 import de.dhbw.myvitality.entities.Employee;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface EmployeeRepository extends CrudRepository<Employee, String> {
-
 }
