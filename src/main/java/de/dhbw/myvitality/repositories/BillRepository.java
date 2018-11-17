4b6643f90adc031ff6b827f16637db1ef3d66bb8
KanBen86 <benjamin.kanzler@gmail.com>
2018-11-17 16:33:43 +0100

    Entfernen der nicht benötigten Controller, Repositories und Services, Kommentar zu den properties hinzugefügt

--- src/main/java/de/dhbw/myvitality/repositories/BillRepository.java
+++
@@ -1,8 +0,0 @@
-package de.dhbw.myvitality.repositories;
-
-import de.dhbw.myvitality.entities.Bill;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface BillRepository extends CrudRepository<Bill, String> {
-}
--- src/main/java/de/dhbw/myvitality/repositories/ContractRepository.java
+++
@@ -1,7 +0,0 @@
-package de.dhbw.myvitality.repositories;
-
-import de.dhbw.myvitality.entities.Contract;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ContractRepository extends CrudRepository<Contract, String> {
-}
